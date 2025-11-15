a59d156cd3d4ab31741ff0c9de3b21871986b1b8
bottomtext228 <yarik.popov2015@yandex.ru>
2025-11-15 22:02:57 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Net;
+
+
+/* var ips = new string[]
+{
+    "1.2.3.5",
+    "1.2.3.4",
+    "1.2.3.9"
+}; */
+
+
+var ips = File.ReadAllLines("data.txt"); 
+
+
+var freeIp = FindLowestFreeIp(ips);
+
+Console.WriteLine(freeIp);
+
+
+static string FindLowestFreeIp(IEnumerable<string> ipStrings)
+{
+    var ips = ipStrings.Select(IpToUint).OrderBy(ip => ip).ToArray();
+
+    for (int i = 0; i < ips.Length - 1; i++)
+    {
+        if (ips[i] + 1 < ips[i + 1] && ((ips[i] + 1) & 0xFF) != 0)
+            return UintToIp(ips[i] + 1);
+    }
+
+    return "No free IP found";
+}
+
+// helpers
+static uint IpToUint(string ip)
+{
+    var bytes = IPAddress.Parse(ip).GetAddressBytes();
+    if (BitConverter.IsLittleEndian)
+        Array.Reverse(bytes);
+    return BitConverter.ToUInt32(bytes, 0);
+}
+
+static string UintToIp(uint ip)
+{
+    var bytes = BitConverter.GetBytes(ip);
+    if (BitConverter.IsLittleEndian)
+        Array.Reverse(bytes);
+    return new IPAddress(bytes).ToString();
+}
